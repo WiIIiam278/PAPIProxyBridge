fbe79c863e8324093c2b2f66dc499f5a673ddd43
William <will27528@gmail.com>
2023-04-17 18:36:11 +0100

    Fix formatFor UUID string formatting

--- common/src/main/java/net/william278/papiproxybridge/user/Request.java
+++ common/src/main/java/net/william278/papiproxybridge/user/Request.java
@@ -42,7 +42,7 @@ public final class Request {
 
     @Override
     public String toString() {
-        return uuid.toString() + message;
+        return uuid.toString() + formatFor.toString() + message;
     }
 
     @NotNull
